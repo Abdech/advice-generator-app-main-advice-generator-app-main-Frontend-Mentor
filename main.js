5130e4f4de7d7ddc88c105d714c1f7db1fc9f08b
Azizsaad <abdech33@gmail.com>
2022-02-28 01:37:56 +0100

    Used the async await instead

--- main.js
+++ main.js
@@ -3,16 +3,18 @@ const theQuote = document.querySelector('.main-quote')
 const dice = document.querySelector('.img-dice')
 
 
-fetch('https://api.adviceslip.com/advice')
-  .then(response => response.json())
-  .then(data => {
-      console.log(data)
-
-      dice.addEventListener('click', () => {
-         quoteNo.innerHTML =   `${data.slip.id}`
-          theQuote.innerHTML = `"${data.slip.advice}"`
-      })
 
 
 
-  });
+      
+async function advicer() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const {slip} = await res.json()
+;
+  console.log(slip);
+  quoteNo.innerText =   `${slip.id}`
+    theQuote.innerText = `"${slip.advice}`
+  
+} 
+
+dice.addEventListener('click', advicer())
