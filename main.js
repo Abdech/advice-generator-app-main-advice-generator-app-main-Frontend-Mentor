2e6517f41898bcb7dbcfb9d909845f3761111aec
Azizsaad <abdech33@gmail.com>
2022-03-04 01:03:06 +0100

    Final update without readme

--- main.js
+++ main.js
@@ -3,18 +3,25 @@ const theQuote = document.querySelector('.main-quote')
 const dice = document.querySelector('.img-dice')
 
 
+function  advicer() {
+fetch('https://api.adviceslip.com/advice')
+  .then(response => response.json())
+  .then(data => {
+    //   console.log(data)
+        quoteNo.innerHTML =   `${data.slip.id}`
+        theQuote.innerHTML = `"${data.slip.advice}"`
+  }).catch(err => theQuote.innerHTML = err);
+}
 
-
-
-      
-async function advicer() {
-    const res = await fetch("https://api.adviceslip.com/advice");
-    const {slip} = await res.json()
-;
-  console.log(slip);
-  quoteNo.innerHTML =   `${slip.id}`
-    theQuote.innerHTML = `"${slip.advice}`
+// This a modern way of using fetch
+// async function advicer() {
+//     const res = await fetch("https://api.adviceslip.com/advice");
+//     const {slip} = await res.json()
+// ;
+//   console.log(slip);
+//   quoteNo.innerHTML =   `${slip.id}`
+//     theQuote.innerHTML = `"${slip.advice}`
   
-} 
+// } 
 
 dice.addEventListener('click', advicer)
